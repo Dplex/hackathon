8bf6e1038c78c5398bc7c44ebb95b08b11fdbc94
dplex <dplex@outlook.com>
2016-04-07 21:35:40 +0900

    first commit

---
+++ Pass-v1.2.1-SampleApp-eclipse/gen/com/samsung/android/sdk/pass/sample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.samsung.android.sdk.pass.sample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
